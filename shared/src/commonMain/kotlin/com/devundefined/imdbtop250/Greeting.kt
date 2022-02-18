f42f425f86cb91c15e1d0301aa4ecf12f3e137df
Oleg Kuzmin <ogkuzmin@gmail.com>
2022-02-18 20:06:37 +0100

    Remove shared module

--- shared/src/commonMain/kotlin/com/devundefined/imdbtop250/Greeting.kt
+++
@@ -1,7 +0,0 @@
-package com.devundefined.imdbtop250
-
-class Greeting {
-    fun greeting(): String {
-        return "Hello, ${Platform().platform}!"
-    }
-}
--- shared/src/commonMain/kotlin/com/devundefined/imdbtop250/Platform.kt
+++
@@ -1,5 +0,0 @@
-package com.devundefined.imdbtop250
-
-expect class Platform() {
-    val platform: String
-}
